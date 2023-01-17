97d2ed5dfa8d2838332af23765f988915625849f
Rodney Morgan <rtm36@1870.uakron.edu>
2023-01-16 20:27:41 -0500

    initial commit

---
+++ components/ConnectToBluetoothText/ConnectToBluetoothText.tsx
@@ -0,0 +1,9 @@
+import { StyleSheet, Text, View } from "react-native";
+
+export default function ConnectToBluetoothText() {
+  return (
+    <View>
+      <Text>Connect to the Totem via Bluetooth</Text>
+    </View>
+  );
+}
